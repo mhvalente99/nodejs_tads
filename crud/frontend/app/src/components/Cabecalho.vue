9bf4f06da4421f193ec3f71b33e97566b31cf84d
Matheus Henrique <matheushenrique@MacBook-Pro-de-Matheus.local>
2018-10-30 20:37:57 -0300

    git status

--- crud/frontend/app/src/components/Cabecalho.vue
+++ crud/frontend/app/src/components/Cabecalho.vue
@@ -1,16 +1,36 @@
 <!-- Essa tag`s nao sao muito utilizadas no vue <template>  dark app - Alinha com o app do App.vue -->
 <template>
   <div id="cabecalho">
-    <v-toolbar color="black darken-3" dark app>
+    <Menu :gaveta="inGaveta"/>
+    <v-toolbar color="black darken-3" dark app fixed>
+      <v-toolbar-title class="ml-0 pl-3" style="width: 300px">
+        <v-toolbar-side-icon @click.stop="inGaveta = !inGaveta" />
+        <span class="hidden-sm-and-down">Bortolato Acabamentos</span>
+        <span class="hidden-sm-and-up">CRUD</span>
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <div class="hidden-sm-and-down">
+        <v-avatar size="32px" tile class="mr-3"> <!-- Do objeto para direita -->
+          <img src="../assets/logo.png" alt="Avatar">
+        </v-avatar>
+
+        <span>Rogerio Campos</span>
+      </div>
     </v-toolbar>
   </div>
 
 </template>
-
 <script>
+import Menu from '@/components/Menu'
 export default {
+  components: {
+    Menu
+  },
   data () {
     return {
+      inGaveta: false
     }
   },
   name: 'Cabecalho'
---
+++ crud/frontend/app/src/components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="menu">
+    <v-navigation-drawer fixed app v-model="gaveta" class="mt-4">
+      <h1>Teste</h1>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props: [
+    'gaveta'
+  ]
+}
+</script>
+
+<style>
+
+</style>
